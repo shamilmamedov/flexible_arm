06b6d74806170d82ca49aa90e885160392314cd5
shamil <shamilnmamedov@gmail.com>
2022-05-30 22:15:33 +0200

    added requirements, sligtly modified code

--- flexible_arm.py
+++ flexible_arm.py
@@ -2,13 +2,7 @@
 
 import numpy as np
 import pinocchio as pin
-from numpy import pi
 import matplotlib.pyplot as plt
-from scipy.integrate import solve_ivp
-
-from animation import Animator
-from controller import DummyController, PDController
-
 
 class FlexibleArm:
     """ Implements flexible arm robot by dividing a link into
@@ -38,7 +32,7 @@ class FlexibleArm:
             assert(D.size==4)
             self.D = np.diag(D)
 
-        path_to_urdf = 'flexible_arm_v1.urdf'
+        path_to_urdf = 'models/flexible_arm_v1.urdf'
         
         # Try to load model from urdf file
         try:
@@ -118,52 +112,3 @@ class FlexibleArm:
         ax.set_ylim([-0.55, 0.55])
         plt.tight_layout()
         plt.show()
-
-
-def robot_ode(t, x, robot, tau):
-    """ Wrapper on ode of the robot class
-    """
-    return robot.ode(x, tau)
-
-def simulate_closed_loop(ts, n_iter, robot, controller, x0):
-    x = np.zeros((n_iter+1, robot.nx))
-    u = np.zeros((n_iter, robot.nu))
-    x[0,:] = x0
-    for k in range(n_iter):
-        qk = x[[k],:robot.nq].T
-        dqk = x[[k],robot.nq:].T
-        
-        tau = controller.compute_torques(qk[0], dqk[0])
-        u[[k],:] = tau
-
-        sol = solve_ivp(robot_ode, [0, ts], x[k,:], args=(robot, tau), vectorized=True)
-        x[k+1,:] = sol.y[:,-1]
-
-    return x, u
-
-
-if __name__ == "__main__":
-    # Create FlexibleArm instance
-    fa = FlexibleArm()
-
-    # Sample a random configuration
-    q = pin.randomConfiguration(fa.model)
-    # fa.visualize(q)
-
-    # Simulate
-    q = np.zeros((fa.nq, 1))
-    dq = np.zeros_like(q)
-    x0 = np.vstack((q, dq))
-
-    # controller = DummyController()
-    controller = PDController(Kp=150, Kd=5, q_ref=np.array([np.pi/4]))
-
-    ts = 0.01
-    n_iter = 150
-    x, u = simulate_closed_loop(ts, n_iter, fa, controller, x0.flatten())    
-
-    # Parse joint positions
-    q = x[:,:fa.nq]
-
-    # Animate simulated motion
-    anim = Animator(fa, q).animate()
