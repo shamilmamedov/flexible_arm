22cfb4290b6fcd2fbe1db07459f1640a8ff89bf4
shamil <shamilnmamedov@gmail.com>
2022-07-12 15:47:56 +0200

    implemented a code for obtaining lumped model (rigid finite element) parameters for a beam. Modified the three segment urdf according to calculated parameters

--- matlab/urdf_to_simscape.m
+++ matlab/urdf_to_simscape.m
@@ -1,5 +1,6 @@
 clc; clear all; close all;
 
-urdf_path = 'models/three_segments/flexible_arm_1dof_3s.urdf';
+% urdf_path = 'models/three_segments/flexible_arm_1dof_3s.urdf';
+urdf_path = 'models/one_segments/flexible_arm_1dof_1s.urdf';
 
 smimport(urdf_path)
--- matlab/visualize.m
+++ matlab/visualize.m
@@ -15,14 +15,14 @@ robot.Gravity = [0 0 -9.81];
 % q = [0.84018772 0.39438293 0.78309922 0.79844003 0.91164736]';
 % q = -0.1*rand(robot.NumBodies-1,1);
 % q = zeros(robot.NumBodies-1,1);
-q = [pi/4, 0, 0]';
+q = [pi/4, -rand, -rand, -rand]';
 
 % Compute gravity and an equilibrium postion of passive joints
 g = gravityTorque(robot, q);
 
-K = diag([100., 100.]);
-q_eq = -inv(K)*g(2:end);
-q = [q(1); q_eq];
+% K = diag([100., 100.]);
+% q_eq = -inv(K)*g(2:end);
+% q = [q(1); q_eq];
 
 getTransform(robot, q,'load')
 show(robot, q)
