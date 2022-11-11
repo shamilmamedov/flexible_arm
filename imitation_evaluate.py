991b179fbfaeed429d37743f8d57da323980ae85
Rudolf <rudolf.reiter@student.tugraz.at>
2022-11-11 15:10:40 +0100

    minor

--- imitation_evaluate.py
+++ imitation_evaluate.py
@@ -3,13 +3,12 @@ from imitation_builder import ImitationBuilder_Wall, ImitationBuilder_Wall2
 from evaluator import Evaluator
 
 if __name__ == "__main__":
-    RUN_EXPERIMENT = False
     PLOT_TRAINING = False
     RUN_EVALUATOR = True
     RENDER = False
-    SHOW_PLOTS = True
+    SHOW_PLOTS = False
 
-    experiment_dir = "data/imitation/wall2/version2"
+    experiment_dir = "data/imitation/wall3/version1"
 
     # plot training
     if PLOT_TRAINING:
@@ -17,7 +16,7 @@ if __name__ == "__main__":
 
     if RUN_EVALUATOR:
         imitation_builder_wall = ImitationBuilder_Wall2()
-        evaluator = Evaluator(builder=imitation_builder_wall, n_episodes=1, policy_dir=experiment_dir,
+        evaluator = Evaluator(builder=imitation_builder_wall, n_episodes=10, policy_dir=experiment_dir,
                               render=RENDER, show_plots=SHOW_PLOTS)
         evaluator.evaluate_nn_safe(policy_dir=experiment_dir)
         evaluator.evaluate_expert()
