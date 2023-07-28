3522a928bafabe3147c4cf94f06f397f26353d08
Erfan Azad <erfan.basiri.azad@gmail.com>
2023-07-28 15:03:21 +0200

    FRA + SB3 + Imitation libraries working together

--- envs/gym_env.py
+++ envs/gym_env.py
@@ -2,12 +2,11 @@
 Implements a gym environment for flexible link robot arm. The environment
 is used for behavioral cloning
 """
-from copy import copy
+from typing import Tuple, Optional
 from dataclasses import dataclass
 
 import gymnasium as gym
 import numpy as np
-from typing import Tuple
 from gymnasium import spaces
 
 from estimator import ExtendedKalmanFilter
@@ -21,32 +20,38 @@ class FlexibleArmEnvOptions:
     """
     Options for imitation learning
     """
+
     dt: float = 0.01
     n_seg: int = 3
     sim_time: float = 3
     qa_start: np.ndarray = np.array([1.5, 0.0, 1.5])
     qa_end: np.ndarray = np.array([1.5, 0.0, 1.5])
     qa_range_start: np.ndarray = np.array([np.pi, np.pi, np.pi])
-    qa_range_end: np.ndarray = np.array([.0, .0, .0])
+    qa_range_end: np.ndarray = np.array([0.0, 0.0, 0.0])
     render_mode = None
     maximum_torques: np.ndarray = np.array([20, 10, 10])
     goal_dist_euclid: float = 0.01
     goal_min_time: float = 1
     sim_noise_R: np.ndarray = None
-    contr_input_states: str = 'estimated'
+    contr_input_states: str = "estimated"
+    render_mode: Optional[str] = None
+
 
 class FlexibleArmEnv(gym.Env):
-    """ Creates gym environment for flexible robot arm
+    """Creates gym environment for flexible robot arm"""
 
-    """
-
-    def __init__(self, options: FlexibleArmEnvOptions, estimator: ExtendedKalmanFilter = None) -> None:
+    def __init__(
+        self,
+        options: FlexibleArmEnvOptions,
+        estimator: ExtendedKalmanFilter = None,
+    ) -> None:
         """
         :parameter n_seg: number of segments per link
         :parameter dt: stepsize of the integrator
         :parameter q0: initial rest configuration of the robot
         """
-        #if estimator is not None:
+        self.render_mode = options.render_mode
+        # if estimator is not None:
         #    assert estimator.x_hat is not None, "Estimator needs to be initialized"
 
         self.options = options
@@ -54,12 +59,14 @@ class FlexibleArmEnv(gym.Env):
         self.dt = options.dt
 
         # initial position
-        self._state, _ = self.sample_rand_config(qa_mean=options.qa_start, 
-                                                 qa_range=options.qa_range_start)
+        self._state, _ = self.sample_rand_config(
+            qa_mean=options.qa_start, qa_range=options.qa_range_start
+        )
 
         # end position
-        self.x_final, self.xee_final = self.sample_rand_config(qa_mean=options.qa_end, 
-                                                               qa_range=options.qa_range_end)
+        self.x_final, self.xee_final = self.sample_rand_config(
+            qa_mean=options.qa_end, qa_range=options.qa_range_end
+        )
 
         # counter for integfration steps and max integration steps
         self.no_intg_steps = 0
@@ -67,37 +74,46 @@ class FlexibleArmEnv(gym.Env):
 
         # Simulator of the model
         sim_opts = SimulatorOptions(
-            dt = self.options.dt,
-            n_iter = self.max_intg_steps,
-            R = self.options.sim_noise_R,
-            contr_input_states = self.options.contr_input_states
+            dt=self.options.dt,
+            n_iter=self.max_intg_steps,
+            R=self.options.sim_noise_R,
+            contr_input_states=self.options.contr_input_states,
         )
-        self.simulator = Simulator(self.model_sym, controller=None, integrator='cvodes',
-                                   estimator=estimator, opts=sim_opts)
+        self.simulator = Simulator(
+            self.model_sym,
+            controller=None,
+            integrator="cvodes",
+            estimator=estimator,
+            opts=sim_opts,
+        )
 
         # Define observation space
         if estimator is None:
             nx_ = self.model_sym.nx
-            self.observation_space = spaces.Box(np.array([-np.pi * 200] * nx_),
-                                                np.array([np.pi * 200] * nx_), 
-                                                dtype=np.float64)
+            self.observation_space = spaces.Box(
+                np.array([-np.pi * 200] * nx_),
+                np.array([np.pi * 200] * nx_),
+                dtype=np.float64,
+            )
         else:
-            self.observation_space = spaces.Box(np.array([-np.pi * 200] * estimator.model.nx),
-                                                np.array([np.pi * 200] * estimator.model.nx), 
-                                                dtype=np.float64)
+            self.observation_space = spaces.Box(
+                np.array([-np.pi * 200] * estimator.model.nx),
+                np.array([np.pi * 200] * estimator.model.nx),
+                dtype=np.float64,
+            )
 
         # Define action space
-        self.action_space = spaces.Box(-options.maximum_torques, options.maximum_torques, 
-                                       dtype=np.float64)
+        self.action_space = spaces.Box(
+            -options.maximum_torques, options.maximum_torques, dtype=np.float64
+        )
 
-        self.render_mode = options.render_mode
         self.goal_dist_counter = 0
         self.stop_if_goal_condition = True
 
         self._state = None
 
     def sample_rand_config(self, qa_mean: np.ndarray, qa_range: np.ndarray):
-        """ Samples a random joint configuration from a given range using
+        """Samples a random joint configuration from a given range using
         uniform distrubution
         """
         qa = np.random.uniform(-qa_range / 2, qa_range / 2) + qa_mean
@@ -107,14 +123,19 @@ class FlexibleArmEnv(gym.Env):
         xee = np.array(self.model_sym.p_ee(q))
         return x[:, 0], xee
 
-    def reset(self):
+    def reset(
+        self, *, seed: Optional[int] = None, options: Optional[dict] = None
+    ) -> Tuple[np.ndarray, dict]:
+        super().reset(seed=seed)
         # Reset state of the robot
         # initial position
-        self._state, _ = self.sample_rand_config(qa_mean=self.options.qa_start,
-                                                 qa_range=self.options.qa_range_start)
+        self._state, _ = self.sample_rand_config(
+            qa_mean=self.options.qa_start, qa_range=self.options.qa_range_start
+        )
         # end position
-        self.x_final, self.xee_final = self.sample_rand_config(qa_mean=self.options.qa_end,
-                                                               qa_range=self.options.qa_range_end)
+        self.x_final, self.xee_final = self.sample_rand_config(
+            qa_mean=self.options.qa_end, qa_range=self.options.qa_range_end
+        )
 
         self.simulator.reset(x0=self._state)
 
@@ -123,32 +144,34 @@ class FlexibleArmEnv(gym.Env):
         self.goal_dist_counter = 0
 
         # Get observations and info
-        if self.options.contr_input_states == 'real':
+        if self.options.contr_input_states == "real":
             observation = self._state
         else:
-            observation = self.simulator.estimator.x_hat[:,0]
-        return observation
+            observation = self.simulator.estimator.x_hat[:, 0]
+        return observation, {}
 
-    def step(self, a) -> Tuple[np.ndarray, float, bool, dict]:
+    def step(
+        self, action: np.ndarray
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, dict]:
         """
         TODO: Make sure actions are in the action space;
-              Clip actions if necessary 
+              Clip actions if necessary
         """
         # TODO ask Rudi why? They should return the same things
         if self.simulator.estimator is None:
-            self._state = self.simulator.step(a)
+            self._state = self.simulator.step(action)
         else:
-            self.simulator.step(a)
+            self.simulator.step(action)
             self._state = self.simulator.x[self.simulator.k, :]
         self.no_intg_steps += 1
 
         # define reward as Euclidian distance to goal
-        x_ee = np.array(self.model_sym.p_ee(self._state[:int(self.model_sym.nq)]))
+        x_ee = np.array(self.model_sym.p_ee(self._state[: int(self.model_sym.nq)]))
         dist = np.linalg.norm(x_ee - self.xee_final, 2)
-        reward = - dist * self.options.dt
+        reward = -dist * self.options.dt
 
         # Check if the state is terminal
-        done = bool(self._terminal(dist))
+        terminated = bool(self._terminal(dist))
 
         # Other outputs
         info = {}
@@ -157,9 +180,11 @@ class FlexibleArmEnv(gym.Env):
         if self.simulator.estimator is None:
             observation = self._state[:, 0]
         else:
-            observation = self.simulator.x_hat[self.no_intg_steps,:]
+            observation = self.simulator.x_hat[self.no_intg_steps, :]
 
-        return (observation, reward, done, info)
+        truncated = self.no_intg_steps >= self.max_intg_steps
+
+        return observation, reward, terminated, truncated, info
 
     def _terminal(self, dist: float):
         if dist < self.options.goal_dist_euclid:
@@ -168,14 +193,21 @@ class FlexibleArmEnv(gym.Env):
             self.goal_dist_counter = 0
 
         done = False
-        if (self.goal_dist_counter >= self.options.goal_min_time / self.options.dt) and self.stop_if_goal_condition:
+        if (
+            self.goal_dist_counter >= self.options.goal_min_time / self.options.dt
+        ) and self.stop_if_goal_condition:
             done = True
         if self.no_intg_steps >= self.max_intg_steps:
             done = True
         return bool(done)
-    
+
     def render(self):
+        if not self.render_mode in ["human", "rgb_array"]:
+            raise ValueError(
+                f"Invalid render_mode: {self.render_mode}. Must be one of ['human', 'rgb_array']"
+            )
+
         if not hasattr(self, "renderer"):
             self.renderer = Panda3dRenderer(self.model_sym.urdf_path)
-        frame = self.renderer.render(q=self._state[:int(self.model_sym.nq)])
+        frame = self.renderer.render(q=self._state[: int(self.model_sym.nq)])
         return frame
--- tests/test_env.py
+++ tests/test_env.py
@@ -3,7 +3,6 @@ RUN COMMAND: python -m tests.test_env
 
 This is a minimal example for testing the FlexibleArmEnv.
 """
-
 from copy import deepcopy
 import numpy as np
 from envs.flexible_arm_3dof import FlexibleArm3DOF
@@ -13,7 +12,7 @@ n_seg = 3
 fa_ld = FlexibleArm3DOF(n_seg)
 
 # Create initial state simulated system
-angle0_0 = 0.
+angle0_0 = 0.0
 angle0_1 = 0
 angle0_2 = np.pi / 20
 
@@ -33,7 +32,15 @@ q_ref[1 + n_seg + 1] += -np.pi / 20
 R_Q = [3e-6] * 3
 R_DQ = [2e-3] * 3
 R_PEE = [1e-4] * 3
-env_options = FlexibleArmEnvOptions(n_seg=n_seg, dt=0.05, qa_start=q0[0:3, 0], qa_end=q_ref[0:3, 0], contr_input_states="real", sim_noise_R=np.diag([*R_Q, *R_DQ, *R_PEE])) # NOTE what should the inputs be here?
+env_options = FlexibleArmEnvOptions(
+    n_seg=n_seg,
+    dt=0.05,
+    qa_start=q0[0:3, 0],
+    qa_end=q_ref[0:3, 0],
+    contr_input_states="real",
+    sim_noise_R=np.diag([*R_Q, *R_DQ, *R_PEE]),
+    render_mode="human",
+)  # NOTE what should the inputs be here?
 env = FlexibleArmEnv(env_options)
 
 # run tests
@@ -42,14 +49,14 @@ use_trained = True
 for i in range(n_eps):
     sum_reward = 0
     print("Episode {}".format(i))
-    done = False
+    terminated = False
     obs = env.reset()
-    while not done:
+    while not terminated:
         action = env.action_space.sample()
         print("Random Action: {}".format(action))
-        (obs, reward, done, info) = env.step(action)
+        (obs, reward, terminated, truncated, info) = env.step(action)
         env.render()
         sum_reward += reward
-        if done:
+        if terminated:
             print("N steps: {}".format(env.no_intg_steps))
     print("Sum of rewards: {}".format(sum_reward))
