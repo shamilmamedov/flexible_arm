d67c83c59c4d24f7c422c5ba46013d5e9fc59162
Erfan Azad <erfan.basiri.azad@gmail.com>
2023-08-15 11:43:06 +0200

    closes #16, #20

--- tests/test_mpc_dagger.py
+++ tests/test_mpc_dagger.py
@@ -4,51 +4,39 @@ RUN COMMAND: python -m tests.test_mpc_dagger
 """
 import tempfile
 import numpy as np
+
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.vec_env import DummyVecEnv
 
-from imitation.algorithms import dagger, bc
-from imitation.data import rollout
-from imitation.data import serialize
+from imitation.algorithms import bc
 from imitation.algorithms.dagger import SimpleDAggerTrainer
+from imitation.data import serialize
 
-from envs.flexible_arm_3dof import FlexibleArm3DOF, get_rest_configuration
 from envs.gym_env import FlexibleArmEnv, FlexibleArmEnvOptions, SymbolicFlexibleArm3DOF
 from mpc_3dof import Mpc3dofOptions, Mpc3Dof
 from utils.gym_utils import CallableMPCExpert
+from utils.utils import StateType
 
 
 rng = np.random.default_rng(0)
 
+
 # --- Create FlexibleArm environment ---
-n_seg = 3
-fa_ld = FlexibleArm3DOF(n_seg)
+n_seg = 5
+n_seg_mpc = 3
 
-# Create initial state simulated system
-qa0 = np.array([np.pi / 2, np.pi / 10, -np.pi / 8])
-q0 = get_rest_configuration(qa0, n_seg)
-dq0 = np.zeros_like(q0)
-x0 = np.vstack((q0, dq0))
-_, xee0 = fa_ld.fk_ee(q0)
-
-# Compute reference
-qa_ref = np.array([0.0, 2 * np.pi / 5, -np.pi / 3])
-q_ref = get_rest_configuration(qa_ref, n_seg)
-dq_ref = np.zeros_like(q_ref)
-x_ref = np.vstack((q_ref, dq_ref))
-_, x_ee_ref = fa_ld.fk_ee(q_ref)
-
-# create environment
-# Measurement noise covairance parameters
+# create data environment
 R_Q = [3e-6] * 3
 R_DQ = [2e-3] * 3
 R_PEE = [1e-4] * 3
 env_options = FlexibleArmEnvOptions(
     n_seg=n_seg,
-    dt=0.05,
-    qa_start=q0[0:3, 0],
-    qa_end=q_ref[0:3, 0],
-    contr_input_states="real",
+    n_seg_estimator=n_seg_mpc,
+    sim_time=1.3,
+    dt=0.01,
+    qa_range_start=np.array([np.pi // 6, np.pi // 6, np.pi // 6]),
+    qa_range_end=np.array([np.pi // 2, np.pi // 2, np.pi // 2]),
+    contr_input_states=StateType.ESTIMATED,  # "real" if the n_seg is the same for the data and control env
     sim_noise_R=np.diag([*R_Q, *R_DQ, *R_PEE]),
     render_mode="human",
 )
@@ -56,25 +44,17 @@ env = FlexibleArmEnv(env_options)
 venv = DummyVecEnv([lambda: env])
 # --------------------------------------
 
-# --- create expert MPC ---
-fa_sym_ld = SymbolicFlexibleArm3DOF(n_seg)
-mpc_options = Mpc3dofOptions(n_seg=n_seg)
-mpc_options.n = 130
-mpc_options.tf = 1.3
-mpc_options.r_diag *= 10
-controller = Mpc3Dof(model=fa_sym_ld, x0=x0, pee_0=xee0, options=mpc_options)
-u_ref = np.zeros(
-    (fa_sym_ld.nu, 1)
-)  # u_ref could be changed to some known value along the trajectory
-
-# Choose one out of two control modes. Reference tracking uses a spline planner.
-controller.set_reference_point(p_ee_ref=x_ee_ref, x_ref=x_ref, u_ref=u_ref)
+# --- Create MPC controller ---
+fa_sym_mpc = SymbolicFlexibleArm3DOF(n_seg_mpc)
+mpc_options = Mpc3dofOptions(n_seg=n_seg_mpc, tf=1.3, n=130)
+controller = Mpc3Dof(model=fa_sym_mpc, x0=None, pee_0=None, options=mpc_options)
 
 # create MPC expert
 expert = CallableMPCExpert(
     controller,
     observation_space=env.observation_space,
     action_space=env.action_space,
+    observation_includes_goal=True,
 )
 # -----------------------------
 
@@ -84,6 +64,8 @@ bc_trainer = bc.BC(
     rng=rng,
 )
 
+rollouts = serialize.load("mpc_expert_rollouts.pkl")
+
 with tempfile.TemporaryDirectory(prefix="dagger_trained_") as tmpdir:
     print(tmpdir)
     dagger_trainer = SimpleDAggerTrainer(
@@ -92,18 +74,19 @@ with tempfile.TemporaryDirectory(prefix="dagger_trained_") as tmpdir:
         expert_policy=expert,
         bc_trainer=bc_trainer,
         rng=rng,
+        expert_trajs=rollouts,
     )
 
     reward, _ = evaluate_policy(
         dagger_trainer.policy,  # type: ignore[arg-type]
         env,
         n_eval_episodes=3,
-        render=True,
+        render=False,
     )
     print(f"Reward before training: {reward}")
 
     print("Training a policy using Dagger")
-    dagger_trainer.train(50)
+    dagger_trainer.train(1000)
 
     reward, _ = evaluate_policy(
         dagger_trainer.policy,  # type: ignore[arg-type]
