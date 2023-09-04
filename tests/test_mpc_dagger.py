3435a6ef2862fe4cd1e889afb3254527922cf13b
Erfan Azad <erfan.basiri.azad@gmail.com>
2023-09-04 10:55:58 +0200

    add seed to dirname

--- tests/test_mpc_dagger.py
+++ tests/test_mpc_dagger.py
@@ -30,7 +30,7 @@ SEED = 0
 
 now = datetime.now()
 LOG_DIR = f"logs/IL/DAGGER/{now.strftime('%Y-%m-%d_%H-%M')}"
-MODEL_DIR = f"trained_models/IL/DAGGER/{now.strftime('%Y-%m-%d_%H-%M')}"
+MODEL_DIR = f"trained_models/IL/DAGGER/{now.strftime('%Y-%m-%d_%H-%M')}/SEED_{SEED}"
 
 rng = np.random.default_rng(SEED)
 seed_everything(SEED)
